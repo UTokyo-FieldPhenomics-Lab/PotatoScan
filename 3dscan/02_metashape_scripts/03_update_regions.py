e58b4156bb17c737ce4930e57a6e9acf10325f45
Howcanoe WANG <howcanoewang@gmail.com>
2024-02-13 11:58:21 +0900

    runScript in xml batch file
    need a script folder with the project

--- 3dscan/02_metashape_scripts/03_update_regions.py
+++ 3dscan/02_metashape_scripts/03_update_regions.py
@@ -1,6 +1,7 @@
 import Metashape
 import sys
 
+import configs as cfg
 from ms_utils import place_region
     
 if __name__ == '__main__':
@@ -20,7 +21,7 @@ if __name__ == '__main__':
 
         if len(sys.argv) > 1 and sys.argv[1] == 'ref':
             # add GCP for z axis
-            chunk.importReference('/home/crest/Documents/Github/PotatoScan/3dscan/02_metashape_scripts/gcp.csv', format=Metashape.ReferenceFormat(3), columns="nxyz", delimiter=",")
+            chunk.importReference(cfg.target_xyz_position_file, format=Metashape.ReferenceFormat(3), columns="nxyz", delimiter=",")
             chunk.updateTransform()
 
         place_region(chunk, (0,0, l/2+h), (0.15,0.15,l), 0)
