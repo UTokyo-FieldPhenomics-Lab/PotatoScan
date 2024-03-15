f301bee6e54bb7746ea702b755b9b1ef4adba1b2
Howcanoe WANG <howcanoewang@gmail.com>
2024-03-15 15:31:51 +0900

    added iter best angle finding

--- 3dscan/03_sfm_rgbd_registration/08_batch_loop.py
+++ 3dscan/03_sfm_rgbd_registration/08_batch_loop.py
@@ -6,8 +6,13 @@ import open3d as o3d
 import copy
 
 from pin_segment import RgbdPinFetcher, SfMPinFetcher
+import linear_algebra as util_la
 from pin_center import find_pin_center
-from icp_align import create_rotational_transform_matrix, paint_pcd_binary, color_based_icp, draw_registration_result
+from icp_align import (
+    paint_pcd_binary, 
+    find_pin_nbr, iter_rotation_angle,
+    color_based_icp, draw_registration_result,
+)
 
 parser = argparse.ArgumentParser(description="Batch script for checking Transform Matrix")
 parser.add_argument('-o', '--overwrite', default=False, action='store_true', help='overwrite existing matrix')
@@ -84,20 +89,50 @@ if __name__ == "__main__":
         ################
         # ICP aligment #
         ################
-        imatrix =  create_rotational_transform_matrix(
+        imatrix =  util_la.create_rotational_transform_matrix(
             rgbd_pin_data['circle_center_3d'], rgbd_pin_data['vector'],
             sfm_pin_data['circle_center_3d'], sfm_pin_data['vector'],
         )
 
+        search_radius = 0.4
+
+        # find the pin neighbour of sfm
+        sfm_nbr_data = find_pin_nbr(sfm_data, sfm_pin_data, search_radius)
+
+        # rotate rgbd according the imatrix to sfm coordinate
+        # with _it suffix
+        rgbd_data_it = {}
+        rgbd_data_it['pcd'] = copy.deepcopy(rgbd_data['pcd']).transform(imatrix)
+        rgbd_data_it['pin_idx'] = rgbd_data['pin_idx']
+        rgbd_data_it['pin_pcd'] = rgbd_data_it['pcd'].select_by_index(rgbd_data['pin_idx'])
+
+        rgbd_pin_data_it = find_pin_center(
+            rgbd_data_it['pin_pcd'], rgbd_data_it['pcd'], 
+            circle_color=[0,0,0], visualize=True, show=False
+        )
+        # find the pin neighbor of rgbd
+        rgbd_nbr_data_it = find_pin_nbr(rgbd_data_it, rgbd_pin_data_it, radius= search_radius)
+
+        best_axis_rotation_matrix = iter_rotation_angle(
+            source_pcd=rgbd_nbr_data_it['nbr_pcd'],
+            target_pcd=sfm_nbr_data['nbr_pcd'],
+            rotate_axis=sfm_pin_data['vector'],
+            rotate_point=sfm_pin_data['circle_center_3d']
+        )
+
+        iimatrix = best_axis_rotation_matrix @ imatrix
+
         o3d.visualization.draw_geometries([
             # initial alignment
             sfm_data['pcd'], copy.deepcopy(rgbd_data['pcd']).transform(imatrix),
+            # after iter rotation match
+            copy.deepcopy(rgbd_data['pcd']).transform(iimatrix).paint_uniform_color([1,0,0])
         ], window_name=f"{pid} - initial registration")
 
         sfm_pcd_bin = paint_pcd_binary(sfm_data['pcd'], sfm_data['pin_idx'])
         rgbd_pcd_bin = paint_pcd_binary(rgbd_data['pcd'], rgbd_data['pin_idx']) 
 
-        tmatrix = color_based_icp(rgbd_pcd_bin, sfm_pcd_bin, imatrix, threshold=0.05)
+        tmatrix = color_based_icp(rgbd_pcd_bin, sfm_pcd_bin, iimatrix, threshold=0.001)
         
         # visualize frame 2
         rgbd_temp, sfm_temp = draw_registration_result(rgbd_data['pcd'], sfm_data['pcd'], tmatrix, paint_color=False, offset=[0.1,0,0])
