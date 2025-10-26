56814e93a9548d654f75908ebc5f04bcb0436f1f
Howcanoe WANG <howcanoewang@gmail.com>
2025-10-26 13:46:58 +0900

    fix(pack): using pathlib to pack zip file

--- 3dscan/04_dataset_scripts/pack_sfm_images.py
+++ 3dscan/04_dataset_scripts/pack_sfm_images.py
@@ -1,31 +1,49 @@
 import os
 import shutil
 from zipfile import ZipFile
+from pathlib import Path
 
 def zip_subfolders(source_folder, target_folder):
+    source_folder = Path(source_folder)
+    target_folder = Path(target_folder)
+
     # Ensure the target folder exists
-    if not os.path.exists(target_folder):
-        os.makedirs(target_folder)
+    if not target_folder.exists():
+        target_folder.mkdir(parents=True)
 
     # Walk through the source folder
-    for root, dirs, files in os.walk(source_folder):
-        for dir_name in dirs:
-            subfolder_path = os.path.join(root, dir_name)
-            zip_file_path = os.path.join(target_folder, f"{dir_name}.zip")
+    for subfolder in source_folder.iterdir():
+        print(f":: zipping folder {subfolder}")
 
-            # Create a zip file for each subfolder
-            with ZipFile(zip_file_path, 'w') as zip_file:
-                for foldername, subdirs, filenames in os.walk(subfolder_path):
-                    for filename in filenames:
-                        file_path = os.path.join(foldername, filename)
-                        # Add file to zip, maintaining folder structure
-                        arcname = os.path.relpath(file_path, subfolder_path)
-                        zip_file.write(file_path, arcname)
-            print(f"Created: {zip_file_path}")
+        if not subfolder.is_dir():
+            print(f"=> skip not directroy [{subfolder}]")
+            continue
+
+        zip_file_path = target_folder / f"{subfolder.name}.zip"
+
+        # Create a zip file for each subfolder
+        with ZipFile(zip_file_path, 'w') as zip_file:
+            for subsubfolder in subfolder.iterdir():
+                print(f"=> zipping cam folder [{subsubfolder.name}]")
+
+                if not subsubfolder.is_dir():
+                    print(f"   -> skip not directory [{subsubfolder}]")
+                    continue
+
+                for file in subsubfolder.iterdir():
+
+                    if not file.is_file():
+                        print(f"   -> skip not file [{subsubfolder}]")
+                        continue
+
+                    arcname = file.relative_to(subfolder)
+                    zip_file.write(file, arcname)
+
+        print(f"Created: {zip_file_path}")
 
 if __name__ == "__main__":
     # source_folder = '/home/crest/w/hwang_Pro/data/2023_hokkaido_potato/images/'
     # target_folder = '/home/crest/Documents/HuggingFace/3DPotatoTwin/2_SfM/0_images/'
     source_folder = '/home/crest/w/hwang_Pro/data/202509_sarabetsu_potato/01_sfm_model/images/'
-    target_folder = '/home/crest/w/hwang_Pro/datasets/HuggingFace/3DPotatoTwin/2_sfm/0_images/'
+    target_folder = '/home/crest/w/hwang_Pro/datasets/3DPotatoTwin.source/2_sfm/0_images/'
     zip_subfolders(source_folder, target_folder)
